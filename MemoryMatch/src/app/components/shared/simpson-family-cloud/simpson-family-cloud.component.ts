38f25c3a45fe7670286da9ae4888785d2d8041fb
Isabella Monsalves <ester.isabella.m@gmail.com>
2024-01-31 00:17:23 -0300

    FEAT: insert img
     image and message of the component are added
    
    issue #17

--- MemoryMatch/src/app/components/shared/simpson-family-cloud/simpson-family-cloud.component.ts
+++ MemoryMatch/src/app/components/shared/simpson-family-cloud/simpson-family-cloud.component.ts
@@ -3,8 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-simpson-family-cloud',
   templateUrl: './simpson-family-cloud.component.html',
-  styleUrls: ['./simpson-family-cloud.component.css']
+  styleUrls: ['./simpson-family-cloud.component.css'],
 })
 export class SimpsonFamilyCloudComponent {
-
+  title = 'familia';
 }
