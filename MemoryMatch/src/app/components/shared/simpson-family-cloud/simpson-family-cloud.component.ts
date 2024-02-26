af85712564a2690f2c4021e96ce37c242349b480
Isabella Monsalves <isamonsalves@MacBook-Pro-de-Isabella.local>
2024-02-26 15:57:06 -0300

    Feat: html progress

--- MemoryMatch/src/app/components/shared/simpson-family-cloud/simpson-family-cloud.component.ts
+++ MemoryMatch/src/app/components/shared/simpson-family-cloud/simpson-family-cloud.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./simpson-family-cloud.component.css'],
 })
 export class SimpsonFamilyCloudComponent {
-  title = 'familia';
+  title = 'family';
 }
