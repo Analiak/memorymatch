f0b79b293a084f93ef81a5c796fae69a96f00bc7
piaacv <122986202+piaacv@users.noreply.github.com>
2024-03-07 14:43:43 -0300

    Button next go to modality view

--- MemoryMatch/src/app/app-routing.module.ts
+++ MemoryMatch/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/views/home/home.component';
+import { ModalityComponent } from './components/views/modality/modality.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent }
+  { path: '', component: HomeComponent },
+  { path: 'Modality', component: ModalityComponent }
 ];
 
 @NgModule({
